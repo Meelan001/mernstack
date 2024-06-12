681e230b00f4b76bb1a5c9c3c6d4b4b5bf59811c
Meelan Magar <mmggrr839@gmail.com>
2024-06-12 13:08:00 +0545

    started a mern stack

---
+++ index.js
@@ -0,0 +1,14 @@
+/* console.log("Hello  world");
+console.log("Mern stack second class is staredx");
+console.log(1+1); */
+// java script is a asynchronus and signle threated language.
+// if two numbers are added it produces a output of sum and if two string are added it concatenate and produces output as a string.
+
+// variable 
+let name="meelan";
+let address= "minbhawan";
+let age =27;
+age=26;
+console.log("my name is "+ name +"." + " i live in " +address +".");
+console.log(`i am ${age} years old.`)
+console.log("a"-"a")
