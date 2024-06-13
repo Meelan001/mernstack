81109a89242ac2c2abfe949c216f1c8b4e5d5add
Meelan Magar <mmggrr839@gmail.com>
2024-06-13 12:45:20 +0545

    loop stareted

--- index.js
+++ index.js
@@ -20,7 +20,61 @@ console.log("hello hello") */
 // we cannot  use double quotes inside double quotes and we also cannot use single quotes 
 //inside single quotes. but qw can use single quotes into the double quotes and double quotes
 //single quotes
-console.log("1"gt+2+3+4)
+// console.log("1"+2+3+4)
 // if we add a number and a string, the interpreter converts the number into string and concatenates both together.
 //we cannot declare same naming variables like let a=24; and let a=26;
+
+// string literals => `` ${}
+
+/* let num1=10;
+let num2=20;
+let message= ` the sum of ${num1} and ${num2} is ${num1 +num2}`;
+console.log(message); */
+
+//boolean truthy and falsy value
+// sinle string is flasy value other are truthy value
+// 0 also is a falsy value other are true.
+/* let a=!!10;
+let name=!!"meelan"
+console.log(a)
+ */
+
+/* if else statement
+if()
+    {
+
+    }
+    else{
+
+    } */
+
+/* let a=true;
+if(a){
+    console.log("iam true")
+}else{
+    console.log("i am false")
+} */
+
+//in if else statemnet condition block is always wraped by parenthesis. () {} []
+
+let firstName= "milan";
+
+
+/* if(firstName==="mila" && firstName==="milan"){
+    console.log("i am milan")
+}else{
+    console.log("i am other")
+}
+
+for(i=0;i<=5;i++){
+    console.log("the number is",i)
+} */
+
+//if value is true single not ! will make value false and double not !! converts value into boolean.
+// let name="meelan";
+// if(!name){
+//     console.log("a")
+// }else{
+//     console.log("b")
+// }
 
