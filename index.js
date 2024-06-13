fb3cf3196af95f3a4b532c54a3dd7e93d57a9237
Meelan Magar <mmggrr839@gmail.com>
2024-06-13 11:52:29 +0545

    second class

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(1+1); */
 // if two numbers are added it produces a output of sum and if two string are added it concatenate and produces output as a string.
 
 // variable 
-let name="meelan";
+/* let name="meelan";
 let address= "minbhawan";
 let age =27;
 age=26;
@@ -13,4 +13,14 @@ console.log("my name is "+ name +"." + " i live in " +address +".");
 console.log(`i am ${age} years old.`)
 console.log("a"-"a")
 
-console.log("hello hello")
+console.log("hello hello") */
+
+// let a='mee"lan'
+// console.log(a)
+// we cannot  use double quotes inside double quotes and we also cannot use single quotes 
+//inside single quotes. but qw can use single quotes into the double quotes and double quotes
+//single quotes
+console.log("1"gt+2+3+4)
+// if we add a number and a string, the interpreter converts the number into string and concatenates both together.
+//we cannot declare same naming variables like let a=24; and let a=26;
+
